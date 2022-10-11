b11b639aa5289bed3a68197916fd5e2b29e59a06
Ndubueze Echefu <nce210002@utdallas.edu>
2022-10-10 22:30:51 -0500

    Lab 3 Update
    - Now it saves custom created cards!
    - You can study more than one flashcard now!

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,8 +8,14 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity() : AppCompatActivity() {
+
+    lateinit var flashcardDatabase: FlashcardDatabase
+    var allFlashcards = mutableListOf<Flashcard>()
+    var currentCardDisplayedIndex = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,6 +24,16 @@ class MainActivity() : AppCompatActivity() {
         val flashcardQuestion = findViewById<TextView>(R.id.flashcard_question)
         val flashcardAnswer = findViewById<TextView>(R.id.flashcard_answer)
         val intent = Intent(this, AddCardActivity::class.java)
+        val nextButton = findViewById<ImageView>(R.id.Next_Arrow)
+
+        flashcardDatabase = FlashcardDatabase(this)
+        allFlashcards = flashcardDatabase.getAllCards().toMutableList()
+
+        if (allFlashcards.size > 0)
+        {
+            flashcardQuestion.text = allFlashcards[0].question
+            flashcardAnswer.text = allFlashcards[0].answer
+        }
 
 
         //Setup
@@ -49,6 +65,20 @@ class MainActivity() : AppCompatActivity() {
                 // Log the value of the strings for easier debugging
                 Log.i("MainActivity", "string1: $string1")
                 Log.i("MainActivity", "string2: $string2")
+
+                //Display newly created flashcard
+                flashcardQuestion.text = string1
+                flashcardAnswer.text = string2
+
+                if (string1 != null && string2 != null)
+                {
+                    //Save to database
+                    flashcardDatabase.insertCard(Flashcard(string1, string2))
+
+                    //Update set of flashcards
+                    allFlashcards = flashcardDatabase.getAllCards().toMutableList()
+                }
+
             } else {
                 Log.i("MainActivity", "Returned null data from AddCardActivity")
             }
@@ -59,6 +89,31 @@ class MainActivity() : AppCompatActivity() {
             resultLauncher.launch(intent)
         }
 
+        nextButton.setOnClickListener {
+            if (allFlashcards.size == 0)
+            {
+                return@setOnClickListener
+            }
+
+            currentCardDisplayedIndex++
+
+            if(currentCardDisplayedIndex >= allFlashcards.size)
+            {
+                Snackbar.make(
+                    flashcardQuestion,
+                    "You've reached the end of the cards, going back to start.",
+                    Snackbar.LENGTH_SHORT)
+                    .show()
+                    currentCardDisplayedIndex = 0
+            }
+
+            allFlashcards = flashcardDatabase.getAllCards().toMutableList()
+            val (question, answer) = allFlashcards[currentCardDisplayedIndex]
+
+            flashcardAnswer.text = answer
+            flashcardQuestion.text = question
         }
 
+
+        }
     }
